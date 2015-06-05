7d4e5c0d6f5c7dc9c1ae2845bb92953b4ffea6aa
jodavies <joshdavies@hotmail.co.uk>
2015-06-05 17:36:23 +0100

    Fixed bugs

--- src/latticeboltzmann.c
+++ src/latticeboltzmann.c
@@ -34,22 +34,13 @@ Arrays indexed: (A(0,0) A(0,1) A(0,2) ...
 #include <CL/opencl.h>
 
 
-
-// Choose precision and vectorization (here choose a serial header)
-//#include "prec_double_avx.h"
-//#include "prec_double_sse.h"
-//#include "prec_double_serial.h"
-//#include "prec_float_avx.h"
-//#include "prec_float_sse.h"
-//#include "prec_float_serial.h"
-
-
 #include "runparams.h"
 
 
 // OpenCL Stuff
 int InitialiseCLEnvironment(cl_platform_id*, cl_device_id*, cl_context*, cl_command_queue*, cl_program*);
 void CleanUpCLEnvironment(cl_platform_id*, cl_device_id*, cl_context*, cl_command_queue*, cl_program*);
+void CheckOpenCLError(cl_int err, int line);
 
 #define MAXDEVICES 2
 #define NQUEUES 1
@@ -74,27 +65,28 @@ double GetWallTime(void);
 int main(void)
 {
 	feenableexcept(FE_ALL_EXCEPT & ~FE_INEXACT);
+	setenv("CUDA_CACHE_DISABLE", "1", 1);
 
 	// Allocate memory. Use _mm_malloc() for alignment. Want 32byte alignment for vector instructions.
 	real_t * f;
 	int * walls;
 
 	int allocSize = NX * NYPADDED * NSPEEDS;
-	printf("Lattice Size: %dx%d (%lf.2 MB)\n", NX, NY, (double)(allocSize*sizeof(*f))/1024.0/1024.0);
+	printf("Lattice Size: %dx%d (%dx%d) (%lf.2 MB)\n", NX,NY, NX,NYPADDED, (double)(allocSize*sizeof(*f))/1024.0/1024.0);
 	f = _mm_malloc(allocSize * sizeof *f, 32);
 	walls = _mm_malloc(NX * NYPADDED * sizeof *walls, 32);
 	InitializeArrays(f, walls);
 
 
 	// Set up OpenCL environment
-	cl_platform_id		platform[2];
-	cl_device_id		device_id[MAXDEVICES];
-	cl_context			context;
-	cl_command_queue	queue[NQUEUES];
-	cl_program			program;
-	cl_kernel			ApplySourceKernelA, ApplySourceKernelB, StreamCollideKernelA, StreamCollideKernelB;
-	cl_int				err;
-	cl_mem 				device_fA, device_fB, device_walls;
+	cl_platform_id    platform[2];
+	cl_device_id      device_id[MAXDEVICES];
+	cl_context        context;
+	cl_command_queue  queue[NQUEUES];
+	cl_program        program;
+	cl_kernel         ApplySourceKernelA, ApplySourceKernelB, StreamCollideKernelA, StreamCollideKernelB;
+	cl_int            err;
+	cl_mem            device_fA, device_fB, device_walls;
 
 	if (InitialiseCLEnvironment(platform, device_id, &context, queue, &program) == EXIT_FAILURE) {
 		printf("Error initialising OpenCL environment\n");
@@ -102,56 +94,39 @@ int main(void)
 	}
 
 
-	//set size of local and global work groups
+	//set size of local and global work groups. want 0th dimension to be the "inner loop"
 	size_t globalSize[2], localSize[2];
-	localSize[0] = 1;
-	localSize[1] = 256;
-	globalSize[0] = localSize[0]*((NX-1)/localSize[0])+localSize[0];
-	globalSize[1] = localSize[1]*((NY-1)/localSize[1])+localSize[1];
-	printf("---OpenCL: Using localSize: %dx%d, globalSize: %dx%d.\n", (int)localSize[0],(int)localSize[1], (int)globalSize[0],(int)globalSize[1]);
+	localSize[0] = 128;
+	localSize[1] = 4;
+	globalSize[0] = localSize[0]*((NY-1)/localSize[0])+localSize[0];
+	globalSize[1] = localSize[1]*((NX-1)/localSize[1])+localSize[1];
+	printf("---OpenCL: Using localSize: %dx%d, globalSize: %dx%d.\n",
+	       (int)localSize[0],(int)localSize[1], (int)globalSize[0],(int)globalSize[1]);
+	//For ApplySourceKernel, only need a 1D kernel
+	size_t localSizeApplySource = 64;
+	size_t globalSizeApplySource = localSizeApplySource*((NX-1)/localSizeApplySource)+localSizeApplySource;
 
 
 	// Create Kernels
 	ApplySourceKernelA = clCreateKernel(program, "ApplySourceKernel", &err);
-	if (err != CL_SUCCESS) {
-		printf("Error in clCreateKernel: %d, line %d.\n", err, __LINE__);
-		return EXIT_FAILURE;
-	}
+	CheckOpenCLError(err, __LINE__);
 	ApplySourceKernelB = clCreateKernel(program, "ApplySourceKernel", &err);
-	if (err != CL_SUCCESS) {
-		printf("Error in clCreateKernel: %d, line %d.\n", err, __LINE__);
-		return EXIT_FAILURE;
-	}
+	CheckOpenCLError(err, __LINE__);
 	StreamCollideKernelA = clCreateKernel(program, "StreamCollideKernel", &err);
-	if (err != CL_SUCCESS) {
-		printf("Error in clCreateKernel: %d, line %d.\n", err, __LINE__);
-		return EXIT_FAILURE;
-	}
+	CheckOpenCLError(err, __LINE__);
 	StreamCollideKernelB = clCreateKernel(program, "StreamCollideKernel", &err);
-	if (err != CL_SUCCESS) {
-		printf("Error in clCreateKernel: %d, line %d.\n", err, __LINE__);
-		return EXIT_FAILURE;
-	}
+	CheckOpenCLError(err, __LINE__);
 
 
 	// Allocate device memory
-	size_t sizeBytes = NX*NY*NSPEEDS * sizeof *f;
+	size_t sizeBytes = NX*NYPADDED*NSPEEDS * sizeof *f;
 	device_fA = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeBytes, NULL, &err);
-	if (err != CL_SUCCESS) {
-		printf("Error allocating memory on device, line %d: %d.\n", __LINE__, err);
-		return EXIT_FAILURE;
-	}
+	CheckOpenCLError(err, __LINE__);
 	device_fB = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeBytes, NULL, &err);
-	if (err != CL_SUCCESS) {
-		printf("Error allocating memory on device, line %d: %d.\n", __LINE__, err);
-		return EXIT_FAILURE;
-	}
-	sizeBytes = NX*NY * sizeof *walls;
+	CheckOpenCLError(err, __LINE__);
+	sizeBytes = NX*NYPADDED * sizeof *walls;
 	device_walls = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeBytes, NULL, &err);
-	if (err != CL_SUCCESS) {
-		printf("Error allocating memory on device, line %d: %d.\n", __LINE__, err);
-		return EXIT_FAILURE;
-	}
+	CheckOpenCLError(err, __LINE__);
 
 
 	// Set kernel arguments
@@ -168,10 +143,7 @@ int main(void)
 	err |= clSetKernelArg(StreamCollideKernelB, 0, sizeof(cl_mem), &device_fB);
 	err |= clSetKernelArg(StreamCollideKernelB, 1, sizeof(cl_mem), &device_fA);
 	err |= clSetKernelArg(StreamCollideKernelB, 2, sizeof(cl_mem), &device_walls);
-	if (err != CL_SUCCESS) {
-		printf("Error in clSetKernelArg, line %d.\n", __LINE__);
-		return EXIT_FAILURE;
-	}
+	CheckOpenCLError(err, __LINE__);
 
 
 	// Start timing here. We include transfers to and from device in timing for comparison with CPU
@@ -179,12 +151,9 @@ int main(void)
 	err = 0;
 
 	// Copy initial arrays to device:
-	err |= clEnqueueWriteBuffer(queue[0], device_fA, CL_TRUE, 0, NX*NY*NSPEEDS*sizeof(real_t), f, 0, NULL, NULL);
-	err |= clEnqueueWriteBuffer(queue[0], device_walls, CL_TRUE, 0, NX*NY*sizeof *walls, walls, 0, NULL, NULL);
-	if (err != CL_SUCCESS) {
-		printf("Error in clEnqueueWriteBuffer, line %d.\n", __LINE__);
-		return EXIT_FAILURE;
-	}
+	err |= clEnqueueWriteBuffer(queue[0], device_fA, CL_TRUE, 0, NX*NYPADDED*NSPEEDS*sizeof *f , f, 0, NULL, NULL);
+	err |= clEnqueueWriteBuffer(queue[0], device_walls, CL_TRUE, 0, NX*NYPADDED*sizeof *walls, walls, 0, NULL, NULL);
+	CheckOpenCLError(err, __LINE__);
 
 	// Begin timesteps
 	for (int n = 0; n < NTIMESTEPS; n+=2) {
@@ -195,7 +164,7 @@ int main(void)
 				double complete = (double)n/(double)NTIMESTEPS;
 				double secElap = GetWallTime()-timeElapsed;
 				double secRem = secElap/complete*(1.0-complete);
-				double avgbw = 4.0*n*sizeof(real_t)*NX*NY*NSPEEDS/(GetWallTime()-timeElapsed)/1024/1024/1024;
+				double avgbw = (2.0*n*sizeof(real_t)*NX*NY*NSPEEDS + 2.0*n*sizeof(real_t)*NX*6)/(GetWallTime()-timeElapsed)/1024/1024/1024;
 				printf("%5.2lf%%--Elapsed: %3dm%02ds, Remaining: %3dm%02ds. [Updates/s: %.3le, Update BW: ~%.3lf GB/s, GFLOPs: ~%.3lf]\n",
 				       complete*100, (int)secElap/60, (int)secElap%60, (int)secRem/60, (int)secRem%60, n/(double)secElap,
 				       avgbw, FLOPPERLATTICEPOINT*NX*NY*n/(double)secElap/1000.0/1000.0/1000.0);
@@ -207,41 +176,33 @@ int main(void)
 		}
 
 		// Do a timestep -- run kernels
-		err |= clEnqueueNDRangeKernel(queue[0], ApplySourceKernelA, 2, NULL, globalSize, localSize, 0, NULL, NULL);
-		if (err != CL_SUCCESS) {
-			printf("Error in clEnqueueNDRangeKernel: %d, line %d.\n", err, __LINE__);
-		return EXIT_FAILURE;
-		}
-		err |= clEnqueueNDRangeKernel(queue[0], StreamCollideKernelA, 2, NULL, globalSize, localSize, 0, NULL, NULL);
-		if (err != CL_SUCCESS) {
-			printf("Error in clEnqueueNDRangeKernel: %d, line %d.\n", err, __LINE__);
-		return EXIT_FAILURE;
-		}
-		err |= clEnqueueNDRangeKernel(queue[0], ApplySourceKernelB, 2, NULL, globalSize, localSize, 0, NULL, NULL);
-		if (err != CL_SUCCESS) {
-			printf("Error in clEnqueueNDRangeKernel: %d, line %d.\n", err, __LINE__);
-		return EXIT_FAILURE;
-		}
-		err |= clEnqueueNDRangeKernel(queue[0], StreamCollideKernelB, 2, NULL, globalSize, localSize, 0, NULL, NULL);
-		if (err != CL_SUCCESS) {
-			printf("Error in clEnqueueNDRangeKernel: %d, line %d.\n", err, __LINE__);
-		return EXIT_FAILURE;
-		}
+		err = clEnqueueNDRangeKernel(queue[0], ApplySourceKernelA, 1, NULL, &globalSizeApplySource, &localSizeApplySource, 0, NULL, NULL);
+		err = clEnqueueNDRangeKernel(queue[0], StreamCollideKernelA, 2, NULL, globalSize, localSize, 0, NULL, NULL);
+		err = clEnqueueNDRangeKernel(queue[0], ApplySourceKernelB, 1, NULL, &globalSizeApplySource, &localSizeApplySource, 0, NULL, NULL);
+		err = clEnqueueNDRangeKernel(queue[0], StreamCollideKernelB, 2, NULL, globalSize, localSize, 0, NULL, NULL);
+
 	}
-	// Copy array back to host
-	clEnqueueReadBuffer(queue[0], device_fA, CL_TRUE, 0, NX*NY*NSPEEDS*sizeof(real_t), f, 0, NULL, NULL);
-	clFinish(queue[0]);
-	timeElapsed = GetWallTime() - timeElapsed;
 	// End iterations
 
+	// Copy array back to host
+	clFinish(queue[0]);
+	err = clEnqueueReadBuffer(queue[0], device_fA, CL_TRUE, 0, NX*NYPADDED*NSPEEDS*sizeof(real_t), f, 0, NULL, NULL);
+	CheckOpenCLError(err, __LINE__);
+
+	// Complete queue and stop timer
+	clFinish(queue[0]);
+	timeElapsed = GetWallTime() - timeElapsed;
+
+
 	// print final run stats
-	double avgbw = 4.0*NTIMESTEPS*sizeof(real_t)*NX*NY*NSPEEDS/timeElapsed/1024/1024/1024;
+	double avgbw = (2.0*NTIMESTEPS*sizeof(real_t)*NX*NY*NSPEEDS + 2.0*NTIMESTEPS*sizeof(real_t)*NX*6)/timeElapsed/1024/1024/1024;
 	printf("100.0%%--Elapsed: %3dm%02ds,                     [Updates/s: %.3le, Update BW: ~%.3lf GB/s, GFLOPs: ~%.3lf]\n",
 	       (int)timeElapsed/60, (int)timeElapsed%60, NTIMESTEPS/timeElapsed, avgbw,
 	       FLOPPERLATTICEPOINT*NX*NY*NTIMESTEPS/timeElapsed/1000.0/1000.0/1000.0);
 	printf("Time: %lf Re %.10le\n", timeElapsed, ComputeReynolds(f, walls));
 
 
+	CleanUpCLEnvironment(platform, device_id, &context, queue, &program);
 	// Free dynamically allocated memory
 	_mm_free(f);
 	_mm_free(walls);
@@ -394,40 +355,28 @@ int InitialiseCLEnvironment(cl_platform_id *platform, cl_device_id *device_id, c
 	}
 	clGetPlatformInfo(platform[0],CL_PLATFORM_VENDOR,sizeof(infostring),infostring,NULL);
 	printf("---OpenCL: Platform Vendor: %s\n",infostring);
+	clGetPlatformInfo(platform[1],CL_PLATFORM_VENDOR,sizeof(infostring),infostring,NULL);
+	printf("---OpenCL: Platform Vendor: %s\n",infostring);
 
 	//get device ID
-//	printf("Getting CL Device IDs...\n");
-	cl_uint totalDevices;
-//	err = clGetDeviceIDs(platform[0], CL_DEVICE_TYPE_CPU, MAXDEVICES, device_id, &totalDevices);
-	err = clGetDeviceIDs(platform[1], CL_DEVICE_TYPE_GPU, MAXDEVICES, device_id, &totalDevices);
-	if (err != CL_SUCCESS) {
-		printf("Error in clGetDeviceIDs, line %d.\n", __LINE__);
-		return EXIT_FAILURE;
-	}
+	err  = clGetDeviceIDs(platform[0], CL_DEVICE_TYPE_ALL, 1, device_id, NULL);
+	err |= clGetDeviceIDs(platform[1], CL_DEVICE_TYPE_ALL, 1, &device_id[1], NULL);
+	CheckOpenCLError(err, __LINE__);
 	char deviceName[50];
-	for (int i = 0; i < (int) totalDevices; i++) {
+	for (int i = 0; i < 2; i++) {
 		clGetDeviceInfo(device_id[i], CL_DEVICE_NAME, sizeof(deviceName), deviceName, NULL);
 		printf("---OpenCL: Device found: %d. %s\n", i, deviceName);
 		cl_ulong maxAlloc;
 		clGetDeviceInfo(device_id[i], CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(maxAlloc), &maxAlloc, NULL);
-		printf("---OpenCL: CL_DEVICE_MAX_MEM_ALLOC_SIZE: %lu MB\n", maxAlloc/1024/1024);
+		printf("---OpenCL:    CL_DEVICE_MAX_MEM_ALLOC_SIZE: %lu MB\n", maxAlloc/1024/1024);
 	}
 
 	//create a context
-//	printf("Creating CL Context...\n");
-	*context = clCreateContext(0, 1, device_id, NULL, NULL, &err);
-	if (err != CL_SUCCESS) {
-		printf("Error in clCreateContext, line %d.\n", __LINE__);
-		return EXIT_FAILURE;
-	}
-
-	//create a queue, CHOOSE DEVICE HERE
-//	printf("Creating CL Queue...\n");
-	*queue = clCreateCommandQueue(*context, device_id[0], 0, &err);
-	if (err != CL_SUCCESS) {
-		printf("Error in clCreateCommandQueue, line %d.\n", __LINE__);
-		return EXIT_FAILURE;
-	}
+	*context = clCreateContext(NULL, 1, &device_id[DEVICE], NULL, NULL, &err);
+	CheckOpenCLError(err, __LINE__);
+	//create a queue
+	*queue = clCreateCommandQueue(*context, device_id[DEVICE], 0, &err);
+	CheckOpenCLError(err, __LINE__);
 
 	//create the program with the source above
 //	printf("Creating CL Program...\n");
@@ -442,7 +391,7 @@ int InitialiseCLEnvironment(cl_platform_id *platform, cl_device_id *device_id, c
 	err = clBuildProgram(*program, 0, NULL, "-I src/runparams.h", NULL, NULL);
 	if (err != CL_SUCCESS) {
 		printf("Error in clBuildProgram: %d, line %d.\n", err, __LINE__);
-		char buffer[2000];
+		char buffer[5000];
 		clGetProgramBuildInfo(*program, device_id[0], CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, NULL);
 		printf("%s\n", buffer);
 		return EXIT_FAILURE;
@@ -453,10 +402,20 @@ int InitialiseCLEnvironment(cl_platform_id *platform, cl_device_id *device_id, c
 }
 
 
+
 void CleanUpCLEnvironment(cl_platform_id *platform, cl_device_id *device_id, cl_context *context, cl_command_queue *queue, cl_program *program)
 {
 	//release CL resources
 	clReleaseProgram(*program);
 	clReleaseCommandQueue(*queue);
 	clReleaseContext(*context);
+}
+
+
+
+void CheckOpenCLError(cl_int err, int line)
+{
+	if (err != CL_SUCCESS) {
+		printf("OpenCL Error %d, line %d\n", err, line);
+	}
 }
--- src/latticeboltzmannkernels.cl
+++ src/latticeboltzmannkernels.cl
@@ -4,31 +4,27 @@
 __kernel void ApplySourceKernel(__global real_t *f,
                                 __global const int *walls)
 {
-	// fetch ids
+	// fetch id
 	int i = get_global_id(0);
-	int j = get_global_id(1);
 
-	if (i < NX) {
+	if (i >= NX) return;
 
-		if (j == 0) {
-			if (walls[I(i,0, 0)] == 0) {
-				if (  (f[I(i,0, 6)] - ACCEL*OMEGA58 > 0.0)
-					 &&(f[I(i,0, 3)] - ACCEL*OMEGA14 > 0.0)
-					 &&(f[I(i,0, 7)] - ACCEL*OMEGA58 > 0.0) ) {
+	if (walls[I(i,0, 0)] == 0) {
+		if (  (f[I(i,0, 6)] - ACCEL*OMEGA58 > 0.0)
+			 &&(f[I(i,0, 3)] - ACCEL*OMEGA14 > 0.0)
+			 &&(f[I(i,0, 7)] - ACCEL*OMEGA58 > 0.0) ) {
 
-					f[I(i,0, 6)] -= ACCEL*OMEGA58;
-					f[I(i,0, 3)] -= ACCEL*OMEGA14;
-					f[I(i,0, 7)] -= ACCEL*OMEGA58;
+			f[I(i,0, 6)] -= ACCEL*OMEGA58;
+			f[I(i,0, 3)] -= ACCEL*OMEGA14;
+			f[I(i,0, 7)] -= ACCEL*OMEGA58;
 
-					f[I(i,0, 5)] += ACCEL*OMEGA58;
-					f[I(i,0, 1)] += ACCEL*OMEGA14;
-					f[I(i,0, 8)] += ACCEL*OMEGA58;
-				}
-
-			}
+			f[I(i,0, 5)] += ACCEL*OMEGA58;
+			f[I(i,0, 1)] += ACCEL*OMEGA14;
+			f[I(i,0, 8)] += ACCEL*OMEGA58;
 		}
 
 	}
+
 
 }
 
@@ -39,85 +35,82 @@ __kernel void StreamCollideKernel(__global const real_t *fSrc,
                                   __global const int *walls)
 {
 	// fetch ids
-	int i = get_global_id(0);
-	int j = get_global_id(1);
+	int i = get_global_id(1);
+	int j = get_global_id(0);
 
-	if (i < NX) {
-	if (j < NY) {
+	if (i >= NX) return;
+	if (j >= NY) return;
 
-		real_t fTmp[NSPEEDS];
+	real_t fTmp[NSPEEDS];
 
-		// pull values from neighbouring lattice points to fTmp
-		int x_u = (i + 1) % NX;
-		int x_d = (i == 0) ? (NX - 1) : (i - 1);
-		int y_r = (j + 1) % NY;
-		int y_l = (j == 0) ? (NY - 1) : (j - 1);
-		fTmp[0] = fSrc[I(i  ,j  , 0)];
-		fTmp[1] = fSrc[I(i  ,y_l, 1)];
-		fTmp[2] = fSrc[I(x_d,j  , 2)];
-		fTmp[3] = fSrc[I(i  ,y_r, 3)];
-		fTmp[4] = fSrc[I(x_u,j  , 4)];
-		fTmp[5] = fSrc[I(x_d,y_l, 5)];
-		fTmp[6] = fSrc[I(x_d,y_r, 6)];
-		fTmp[7] = fSrc[I(x_u,y_r, 7)];
-		fTmp[8] = fSrc[I(x_u,y_l, 8)];
+	// pull values from neighbouring lattice points to fTmp
+	int x_u = (i + 1) % NX;
+	int x_d = (i == 0) ? (NX - 1) : (i - 1);
+	int y_r = (j + 1) % NY;
+	int y_l = (j == 0) ? (NY - 1) : (j - 1);
+	fTmp[0] = fSrc[I(i  ,j  , 0)];
+	fTmp[1] = fSrc[I(i  ,y_l, 1)];
+	fTmp[2] = fSrc[I(x_d,j  , 2)];
+	fTmp[3] = fSrc[I(i  ,y_r, 3)];
+	fTmp[4] = fSrc[I(x_u,j  , 4)];
+	fTmp[5] = fSrc[I(x_d,y_l, 5)];
+	fTmp[6] = fSrc[I(x_d,y_r, 6)];
+	fTmp[7] = fSrc[I(x_u,y_r, 7)];
+	fTmp[8] = fSrc[I(x_u,y_l, 8)];
 
 
-		// bounce-back from wall
-		if (walls[I(i,j, 0)] == 1) {
-			fDst[I(i,j, 1)] = fTmp[3];
-			fDst[I(i,j, 2)] = fTmp[4];
-			fDst[I(i,j, 3)] = fTmp[1];
-			fDst[I(i,j, 4)] = fTmp[2];
-			fDst[I(i,j, 5)] = fTmp[7];
-			fDst[I(i,j, 6)] = fTmp[8];
-			fDst[I(i,j, 7)] = fTmp[5];
-			fDst[I(i,j, 8)] = fTmp[6];
-		}
-
-		else {
-			real_t density = 0;
-			for (int s = 0; s < NSPEEDS; s++) {
-				density += fTmp[s];
-			}
-
-			real_t u_x = (+(fTmp[6]+fTmp[2]+fTmp[5])
-							  -(fTmp[7]+fTmp[4]+fTmp[8]))/density;
-			real_t u_y = (+(fTmp[5]+fTmp[1]+fTmp[8])
-							  -(fTmp[6]+fTmp[3]+fTmp[7]))/density;
-
-			real_t uDotu = u_x * u_x + u_y * u_y;
-
-			// Directional velocity components e_i dot u
-			real_t u[NSPEEDS];
-			u[1] =     +u_y;
-			u[2] = +u_x;
-			u[3] =     -u_y;
-			u[4] = -u_x;
-			u[5] = +u_x+u_y;
-			u[6] = +u_x-u_y;
-			u[7] = -u_x-u_y;
-			u[8] = -u_x+u_y;
-
-			// equilibrium density
-			real_t fequ[NSPEEDS];
-			fequ[0] = OMEGA0 * density * (1 - 3.0/2.0*uDotu/CSQ);
-			fequ[1] = OMEGA14* density * (1 + 3.0*u[1]/CSQ +9.0/2.0*u[1]*u[1]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
-			fequ[2] = OMEGA14* density * (1 + 3.0*u[2]/CSQ +9.0/2.0*u[2]*u[2]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
-			fequ[3] = OMEGA14* density * (1 + 3.0*u[3]/CSQ +9.0/2.0*u[3]*u[3]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
-			fequ[4] = OMEGA14* density * (1 + 3.0*u[4]/CSQ +9.0/2.0*u[4]*u[4]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
-			fequ[5] = OMEGA58* density * (1 + 3.0*u[5]/CSQ +9.0/2.0*u[5]*u[5]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
-			fequ[6] = OMEGA58* density * (1 + 3.0*u[6]/CSQ +9.0/2.0*u[6]*u[6]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
-			fequ[7] = OMEGA58* density * (1 + 3.0*u[7]/CSQ +9.0/2.0*u[7]*u[7]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
-			fequ[8] = OMEGA58* density * (1 + 3.0*u[8]/CSQ +9.0/2.0*u[8]*u[8]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
-
-			// relaxation:
-			for (int s = 0; s < NSPEEDS; s++) {
-				fDst[I(i,j, s)] = fTmp[s] + (1.0/TAU)*(fequ[s] - fTmp[s]);
-			}
-		}
-
+	// bounce-back from wall
+	if (walls[I(i,j, 0)] == 1) {
+		fDst[I(i,j, 1)] = fTmp[3];
+		fDst[I(i,j, 2)] = fTmp[4];
+		fDst[I(i,j, 3)] = fTmp[1];
+		fDst[I(i,j, 4)] = fTmp[2];
+		fDst[I(i,j, 5)] = fTmp[7];
+		fDst[I(i,j, 6)] = fTmp[8];
+		fDst[I(i,j, 7)] = fTmp[5];
+		fDst[I(i,j, 8)] = fTmp[6];
 	}
+
+	else {
+		real_t density = 0;
+		for (int s = 0; s < NSPEEDS; s++) {
+			density += fTmp[s];
+		}
+
+		real_t u_x = (+(fTmp[6]+fTmp[2]+fTmp[5])
+						  -(fTmp[7]+fTmp[4]+fTmp[8]))/density;
+		real_t u_y = (+(fTmp[5]+fTmp[1]+fTmp[8])
+						  -(fTmp[6]+fTmp[3]+fTmp[7]))/density;
+
+		real_t uDotu = u_x * u_x + u_y * u_y;
+
+		// Directional velocity components e_i dot u
+		real_t u[NSPEEDS];
+		u[1] =     +u_y;
+		u[2] = +u_x;
+		u[3] =     -u_y;
+		u[4] = -u_x;
+		u[5] = +u_x+u_y;
+		u[6] = +u_x-u_y;
+		u[7] = -u_x-u_y;
+		u[8] = -u_x+u_y;
+
+		// equilibrium density
+		real_t fequ[NSPEEDS];
+		fequ[0] = OMEGA0 * density * (1 - 3.0/2.0*uDotu/CSQ);
+		fequ[1] = OMEGA14* density * (1 + 3.0*u[1]/CSQ +9.0/2.0*u[1]*u[1]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
+		fequ[2] = OMEGA14* density * (1 + 3.0*u[2]/CSQ +9.0/2.0*u[2]*u[2]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
+		fequ[3] = OMEGA14* density * (1 + 3.0*u[3]/CSQ +9.0/2.0*u[3]*u[3]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
+		fequ[4] = OMEGA14* density * (1 + 3.0*u[4]/CSQ +9.0/2.0*u[4]*u[4]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
+		fequ[5] = OMEGA58* density * (1 + 3.0*u[5]/CSQ +9.0/2.0*u[5]*u[5]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
+		fequ[6] = OMEGA58* density * (1 + 3.0*u[6]/CSQ +9.0/2.0*u[6]*u[6]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
+		fequ[7] = OMEGA58* density * (1 + 3.0*u[7]/CSQ +9.0/2.0*u[7]*u[7]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
+		fequ[8] = OMEGA58* density * (1 + 3.0*u[8]/CSQ +9.0/2.0*u[8]*u[8]/CSQ/CSQ -3.0/2.0*uDotu/CSQ);
+
+		// relaxation:
+		for (int s = 0; s < NSPEEDS; s++) {
+			fDst[I(i,j, s)] = fTmp[s] + (1.0/TAU)*(fequ[s] - fTmp[s]);
+		}
 	}
 
 }
